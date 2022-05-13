8766d993b9dce788d3c579d5d527cde776693890
Eugene Zhygliy <zhygliy@itirra.com>
2022-05-13 10:59:03 +0300

    chore(config.helmet): add settings to allow swagger

--- apps/api-server/src/config/helmet.config.ts
+++ apps/api-server/src/config/helmet.config.ts
@@ -3,10 +3,12 @@ import { registerAs } from '@nestjs/config';
 export const helmetConfig = registerAs('helmet', (): unknown => ({
   contentSecurityPolicy: {
     directives: {
-      defaultSrc: ["'self'", "'unsafe-inline'", '*.googleapis.com', '*.gstatic.com', 'cdn.jsdelivr.net'],
-      // styleSrc: ["'self'", "'unsafe-inline'"],
-      // imgSrc: ["'self'", 'data:', 'validator.swagger.io'],
-      // scriptSrc: ["'self'", "https: 'unsafe-inline'"],
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", 'data:', 'validator.swagger.io'],
+        scriptSrc: ["'self'", "https: 'unsafe-inline'"],
+      },
     },
   },
 }));

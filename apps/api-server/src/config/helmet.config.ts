3a2017e7faf387be714374105e43afce86217613
Eugene Zhygliy <zhygliy@itirra.com>
2022-05-13 11:12:10 +0300

    fix(config.helmet): typo

--- apps/api-server/src/config/helmet.config.ts
+++ apps/api-server/src/config/helmet.config.ts
@@ -3,12 +3,10 @@ import { registerAs } from '@nestjs/config';
 export const helmetConfig = registerAs('helmet', (): unknown => ({
   contentSecurityPolicy: {
     directives: {
-      directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", 'data:', 'validator.swagger.io'],
-        scriptSrc: ["'self'", "https: 'unsafe-inline'"],
-      },
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", 'data:', 'validator.swagger.io'],
+      scriptSrc: ["'self'", "https: 'unsafe-inline'"],
     },
   },
 }));

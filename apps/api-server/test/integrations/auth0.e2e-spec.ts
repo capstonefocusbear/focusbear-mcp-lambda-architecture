c8ca0d708e7929f0406684bfa8a9397dcb198028
Eugene Zhygliy <zhygliy@itirra.com>
2022-05-18 17:23:16 +0300

    test(integration): auth0 login

---
+++ apps/api-server/test/integrations/auth0.e2e-spec.ts
@@ -0,0 +1,47 @@
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import * as jwt from 'jsonwebtoken';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../../src/app.module';
+import { auth0LoginUser } from '../utils/auth0-login';
+
+describe('Auth0', () => {
+  let app: NestFastifyApplication;
+  const testUser = {
+    email: 'evgeniy.zhygliy@gmail.com',
+    password: '1233218293Wer*7',
+  };
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
+
+    await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  /**
+   * 1) It requires disabled MFA in Auth0
+   * 2) "On-login action" in Auth0 will call endpoint setted in Auth0
+   * "Dashboard/actions/library/custom/Add user data in access Token"
+   * (pay attention to it)
+   * 3) "On-login action" does not work with localhost, so that Auth0 will call remote server
+   */
+  describe('Get accessToken with UserAuthMetadata', () => {
+    it('positive: should return access token with user-auth-metadata inside', async () => {
+      const tokenData = await auth0LoginUser(testUser.email, testUser.password);
+      console.log({ tokenData });
+
+      const payload = jwt.decode(tokenData.access_token);
+
+      expect(tokenData).toBeDefined();
+      expect(payload).toBeDefined();
+    });
+  });
+});

4c2ec666fa772ea5640f8a5b63f606fdf42459f6
Eugene Zhygliy <zhygliy@itirra.com>
2022-06-06 11:10:11 +0300

    fix(): add default encryption secret to run tests

--- apps/api-server/src/config/typeorm-encryption.config.ts
+++ apps/api-server/src/config/typeorm-encryption.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { scryptSync } from 'crypto';
 
 export const typeormEncryptionConfig = (salt = 'localSalt') => ({
-  key: scryptSync(process.env.TYPEORM_ENCRYPTION_KEY, salt.split('').reverse().join('salt'), 32).toString('hex'),
+  key: scryptSync(process.env.TYPEORM_ENCRYPTION_KEY ?? '', salt.split('').reverse().join('salt'), 32).toString('hex'),
   algorithm: 'aes-256-cbc',
   ivLength: 16,
 });

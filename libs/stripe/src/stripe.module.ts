0a458ff7d79263cd51cfc33191904a16bb182ab6
Eugene Zhygliy <zhygliy@itirra.com>
2022-08-16 09:31:21 +0300

    refactor(stripe): dynamic import

--- libs/stripe/src/stripe.module.ts
+++ libs/stripe/src/stripe.module.ts
@@ -1,5 +1,5 @@
-import { DynamicModuleFactory } from '@app/dynamic-module';
 import { Module } from '@nestjs/common';
+import { DynamicModuleFactory } from '../../dynamic-module/src';
 import { IStripeOptions } from './interfaces';
 import { STRIPE_MODULE_OPTIONS } from './stripe.constants';
 import { StripeService } from './stripe.service';

6013f36ca11f8af005454874be404e98ff5089a2
Eugene Zhygliy <zhygliy@itirra.com>
2022-08-19 11:50:19 +0300

    feat(stripe): return urls instead of redirection

--- apps/api-server/src/modules/subscription/controllers/webhooks/stripe.controller.ts
+++ apps/api-server/src/modules/subscription/controllers/webhooks/stripe.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Redirect, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { AuthContext } from '../../../../shared/decorators/passport.decorator';
 import { StripeService } from '../../../../../../../libs/stripe/src';
@@ -14,16 +14,14 @@ export class StripeController {
   constructor(private readonly stripeService: StripeService) {}
 
   @Post('create-checkout-session')
-  @Redirect()
   async createCheckoutSession(@Body() { price_id }: CreateStripeCheckoutSessionDto, @AuthContext() { user }: Passport) {
     const session = await this.stripeService.createCheckoutSession(price_id, user.stripeCustomerId);
-    return { url: session.url, statusCode: 303 };
+    return { url: session.url };
   }
 
   @Post('create-portal-session')
-  @Redirect()
   async createPortalSession(@AuthContext() { user }: Passport) {
     const session = await this.stripeService.createPortalSession(user.stripeCustomerId);
-    return { url: session.url, statusCode: 303 };
+    return { url: session.url };
   }
 }

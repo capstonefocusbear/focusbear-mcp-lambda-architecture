c5954c847948592f3f298b2ee5f64cb964b8f1bb
Eugene Zhygliy <zhygliy@itirra.com>
2022-08-19 10:08:35 +0300

    chore(): enable cors

--- apps/api-server/src/main.ts
+++ apps/api-server/src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'fastify-helmet';
+import cors from 'fastify-cors';
 import fastifyRawBody from 'fastify-raw-body';
 import { AppModule } from './app.module';
 import { TypeOrmExceptionFilter } from './shared/exceptions/type-orm-exception.filter';
@@ -34,6 +35,7 @@ async function bootstrap(): Promise<void> {
   app.useGlobalPipes(new ValidationPipe(VALIDATION_PIPE));
   app.useGlobalFilters(new TypeOrmExceptionFilter());
   app.register(helmet, HELMET);
+  app.register(cors);
   app.register(fastifyRawBody, { global: true }); // turn off global and set route spesific // routes: ['/subscription/webhooks/stripe']
 
   bootstrapApiDocumentation(app);

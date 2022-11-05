6bcbc81f1ceb6ba7cf0540b2bfa696969c3cadad
deonvisser44 <deonvisser44@gmail.com>
2022-11-05 11:35:51 +0700

    fix(): configure DB connection for cron job

--- pusher-cron-job.ts
+++ pusher-cron-job.ts
@@ -9,28 +9,18 @@ const { DateTime } = require('luxon');
 
 dotenv.config();
 
-dotenv.config();
+const connectionString = process.env.RENDER_DB_CONNECTION_URI;
 
 const fetchNotifications = async () => {
-  try {
-    const pool = new Pool({
-      user: process.env.POSTGRES_USERNAME,
-      host: process.env.POSTGRES_HOST,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-      port: Number(process.env.POSTGRES_PORT) || 5432,
-    });
-    await pool.connect();
-    const currentTime = DateTime.now().toISO();
-    const timeInFifiteenMinutes = DateTime.now().plus({ minutes: 15 }).toISO();
-    const res = await pool.query({
-      text: 'SELECT * FROM notifications WHERE event_begins >= $1 AND event_begins <= $2 AND received IS NOT TRUE;',
-      values: [currentTime, timeInFifiteenMinutes],
-    });
-    return res.rows;
-  } catch (error) {
-    console.error(error);
-  }
+  const pool = new Pool({ connectionString });
+  await pool.connect();
+  const currentTime = DateTime.now().toISO();
+  const timeInFifiteenMinutes = DateTime.now().plus({ minutes: 15 }).toISO();
+  const res = await pool.query({
+    text: 'SELECT * FROM notifications WHERE event_begins >= $1 AND event_begins <= $2 AND received IS NOT TRUE;',
+    values: [currentTime, timeInFifiteenMinutes],
+  });
+  return res.rows;
 };
 
 const beamsClient = new PushNotifications({
@@ -56,16 +46,22 @@ const sendBeamsPushNotification = async (userId: string, notificationData: Notif
 };
 
 (async () => {
-  const notificationsToSend = await fetchNotifications();
-  if (notificationsToSend.length === 0) return;
-  notificationsToSend.forEach((notification) => {
-    const { id, summary, description, event_begins, event_ends } = notification;
-    sendBeamsPushNotification(notification.user_id, {
-      id,
-      summary,
-      description,
-      event_begins,
-      event_ends,
+  try {
+    const notificationsToSend = await fetchNotifications();
+    console.log(`Ran for ${notificationsToSend.length} notification(s).`);
+    if (notificationsToSend.length === 0) process.exit();
+    notificationsToSend.forEach(async (notification) => {
+      const { id, summary, description, event_begins, event_ends } = notification;
+      await sendBeamsPushNotification(notification.user_id, {
+        id,
+        summary,
+        description,
+        event_begins,
+        event_ends,
+      });
     });
-  });
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
 })();

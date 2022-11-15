7d2b53ac288b6ad0a4ffad438b4d265f1c059a95
Amanuel Tesfaye <amanueltsfy@gmail.com>
2022-11-15 08:54:36 +0300

    fix(): update week summary to support all type of log summary types

--- apps/api-server/src/modules/activity/repositories/completed-activity.repository.ts
+++ apps/api-server/src/modules/activity/repositories/completed-activity.repository.ts
@@ -109,7 +109,7 @@ export class CompletedActivityRepository extends BaseRepository<CompletedActivit
     });
   }
 
-  async getWeekSummaryTotal(user_id: string, start_date: DateTime, end_date: DateTime): Promise<CompletedActivity[]> {
+  async getWeekSummary(user_id: string, start_date: DateTime, end_date: DateTime): Promise<CompletedActivity[]> {
     return this.orm.find({
       where: {
         user_id,
@@ -117,10 +117,10 @@ export class CompletedActivityRepository extends BaseRepository<CompletedActivit
         activity: {
           log_quantity: true,
           type: ActivityType.break,
-          log_summary_type: LogSummaryType.SUM,
         },
       },
       relations: ['activity'],
+      select: ['finish_time', 'quantity_logged'],
     });
   }
 

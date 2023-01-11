e810f8a0011548470d013dfde4a99d3815f186d6
deonvisser44 <deonvisser44@gmail.com>
2023-01-11 19:47:14 +0700

    fix: move fetching user from queue to service

--- apps/api-server/src/modules/events/consumers/events.consumer.ts
+++ apps/api-server/src/modules/events/consumers/events.consumer.ts
@@ -2,23 +2,20 @@ import { Process, Processor } from '@nestjs/bull';
 import { InjectSentry, SentryService } from '@ntegral/nestjs-sentry';
 import { Job } from 'bull';
 import * as axios from 'axios';
-import { NotFoundException } from '@nestjs/common';
 import { SendinblueService } from '../../../../../../libs/sendinblue/src/sendinblue.service';
-import { UserRepository } from '../../user/repositories/user.repository';
 import { TrackEventDto } from '../dto/track-event.dto';
 
 @Processor('events')
 export class EventsConsumer {
   constructor(
     @InjectSentry() private readonly sentryService: SentryService,
-    private readonly userRepository: UserRepository,
     private readonly sendinblueService: SendinblueService,
   ) {}
 
   @Process('track-event')
-  async readOperationJob(job: Job<{ trackEventDto: TrackEventDto; user_id: string }>) {
+  async readOperationJob(job: Job<{ trackEventDto: TrackEventDto; user_id: string; email: string }>) {
     const {
-      data: { user_id, trackEventDto },
+      data: { user_id, email, trackEventDto },
     } = job;
     try {
       this.sentryService.instance().addBreadcrumb({
@@ -30,9 +27,7 @@ export class EventsConsumer {
           treack_event: trackEventDto,
         },
       });
-      const user = await this.userRepository.orm.findOneBy({ id: user_id });
-      if (!user) throw new NotFoundException(`User with ID: ${user_id} does not exist!`);
-      await this.sendinblueService.registerSendinblueEvent(user.email, trackEventDto);
+      await this.sendinblueService.registerSendinblueEvent(email, trackEventDto);
     } catch (error) {
       this.sentryService.instance().captureMessage(JSON.stringify(error), 'error');
       await axios.default.post(process.env.SLACK_BACKEND_ALERTS_WEBHOOK, {

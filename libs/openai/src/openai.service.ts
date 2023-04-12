988b7a46e03ca9d153bec167b8732616b2c24cc3
deonvisser44 <deonvisser44@gmail.com>
2023-04-12 13:00:58 +0700

    chore: update openai chat prompt

--- libs/openai/src/openai.service.ts
+++ libs/openai/src/openai.service.ts
@@ -60,22 +60,27 @@ export class OpenAIService {
       });
       const config = new Configuration({ ...this.options });
       const openai = new OpenAIApi(config);
-      const defaultChat: ChatCompletionRequestMessage[] = [
-        {
-          role: 'system',
-          content: `You are a ${language} speaking chatbot(don't mention that you are a chatbot) named Focus Bear helping people to be productive and achieve the goals they set out to achieve. You are restricted to talking about productivity and habits. Please greet the user briefly.`,
-        },
-      ];
+      const defaultChat: ChatCompletionRequestMessage = {
+        role: 'system',
+        content: `You are a ${language} speaking chatbot(don't mention that you are a chatbot) 
+        named Focus Bear helping people to be productive and achieve 
+        the goals they set out to achieve. You are part of an app that has features 
+        like allowing users to block apps and websites they find distracting and letting them 
+         practice habits they set out to do as part of their daily routines. You are restricted to 
+        talking about productivity and habits and should limit responses to 100 words. Please greet the user briefly.`,
+      };
       const chatHistory: ChatCompletionRequestMessage[] = [...messages];
       const hasExistingChat = Boolean(messages.length);
+      if (!hasExistingChat) {
+        chatHistory.push(defaultChat);
+      }
       const fetchChatCompletion = async () => {
         let retryCount = 0;
         while (retryCount < 3) {
           try {
             const completions = await openai.createChatCompletion({
               model: 'gpt-3.5-turbo',
-              messages: hasExistingChat ? messages : defaultChat,
-              max_tokens: 100,
+              messages: chatHistory,
               temperature: 0.7,
               n: 1,
             });

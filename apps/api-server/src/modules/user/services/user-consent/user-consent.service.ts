0011455c19a271f64c4c8b9bbe98c818bbc44557
deonvisser44 <deonvisser44@gmail.com>
2023-05-16 15:58:56 +0700

    chore(user-consent): refactor if statement

--- apps/api-server/src/modules/user/services/user-consent/user-consent.service.ts
+++ apps/api-server/src/modules/user/services/user-consent/user-consent.service.ts
@@ -17,11 +17,8 @@ export class UserConsentService {
     if (!user) throw new NotFoundException(`User with id: ${user_id} does not exist!`);
     const { consent_type, consent_status } = userConsent;
     const existingConsentRecord = await this.userConsentRepository.orm.findOneBy({ consent_type, user_id });
-    if (consent_type === UserConsentTypes.TERMS_OF_SERVICE && !!consent_status) {
-      await this.userRepository.orm.update({ id: user_id }, { has_consented_to_terms_of_service: true });
-    }
-    if (consent_type === UserConsentTypes.TERMS_OF_SERVICE && !consent_status) {
-      await this.userRepository.orm.update({ id: user_id }, { has_consented_to_terms_of_service: false });
+    if (consent_type === UserConsentTypes.TERMS_OF_SERVICE) {
+      await this.userRepository.orm.update({ id: user_id }, { has_consented_to_terms_of_service: !!consent_status });
     }
     if (existingConsentRecord) {
       // if user is revoking consent, update withdrawal date

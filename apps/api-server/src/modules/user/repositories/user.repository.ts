550425ecd70fd014e2046ecade89db95c840ad66
deonvisser44 <deonvisser44@gmail.com>
2023-06-19 15:31:21 +0700

    chore: include focus tags in user details response

--- apps/api-server/src/modules/user/repositories/user.repository.ts
+++ apps/api-server/src/modules/user/repositories/user.repository.ts
@@ -146,6 +146,7 @@ export class UserRepository extends BaseRepository<User> {
       .createQueryBuilder('users')
       .leftJoinAndSelect('users.devices', 'devices')
       .leftJoinAndSelect('users.focus_modes', 'focus_modes')
+      .leftJoinAndSelect('focus_modes.tags', 'tags')
       .where('users.id = :id', { id })
       .getOne();
   }

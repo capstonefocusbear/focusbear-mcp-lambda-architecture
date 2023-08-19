af0519bdebd7a67d57268e4359c167505c2eea3e
deonvisser44 <deonvisser44@gmail.com>
2023-08-19 22:26:24 +0700

    chore: return failed activities when doing offline sync

--- apps/api-server/src/modules/activity/dto/sync-offline-activity.dto.ts
+++ apps/api-server/src/modules/activity/dto/sync-offline-activity.dto.ts
@@ -1,5 +1,4 @@
 import { User } from '../../user/entities/user.entity';
-import { CompletedActivityResponse } from '../domain/completed-activity-response.model';
 import { ActivitySequence } from '../entities/activity-sequence.entity';
 import { Activity } from '../entities/activity.entity';
 import { CreateCompletedActivityDto } from './create-completed-activity.dto';
@@ -13,6 +12,4 @@ export class SyncOfflineActivityArgs {
   allActivitiesFromSequence: Activity[];
 
   sequence: ActivitySequence;
-
-  createdLogs: CompletedActivityResponse[];
 }

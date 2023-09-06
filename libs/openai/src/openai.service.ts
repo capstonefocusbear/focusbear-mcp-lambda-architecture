1e0d4bcd5eb054b2501d7f744bec40f178e2855c
deonvisser44 <51429368+deonvisser44@users.noreply.github.com>
2023-09-06 14:08:32 +0700

    chore: improve username validation (#517)

--- libs/openai/src/openai.service.ts
+++ libs/openai/src/openai.service.ts
@@ -329,7 +329,8 @@ export class OpenAIService {
     const openai = new OpenAIApi(config);
     const defaultChat: ChatCompletionRequestMessage = {
       role: 'system',
-      content: `Given the following username, determine whether it uses curse words or could be offensive to anyone, if it is deemed fine, return true, if offensive, return false.
+      content: `Given the following username, determine whether it uses curse words, sexual language, or could be offensive to anyone, if it is deemed fine, return true, if offensive, return false.
+      Examples of inappropriate usernames for which false should be returned: sexymommee, hitler 
       the output should be in the format:
       { allowed: boolean }
       username: ${username}

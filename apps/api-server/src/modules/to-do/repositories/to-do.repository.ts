5f502fb4baabacee63fe9d7660fbcc23ab2260c2
deonvisser44 <deonvisser44@gmail.com>
2023-10-31 12:03:04 +0700

    fix: fetching to do subtasks

--- apps/api-server/src/modules/to-do/repositories/to-do.repository.ts
+++ apps/api-server/src/modules/to-do/repositories/to-do.repository.ts
@@ -27,6 +27,7 @@ export class ToDoRepository extends BaseRepository<ToDo> {
         'to_do.external_task_id',
         'to_do.external_task_metadata',
         'to_do.created_at',
+        'to_do.subtasks',
         'tags.id',
         'tags.text',
       ])

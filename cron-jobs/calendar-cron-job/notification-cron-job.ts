650bf655a4dcd8af2bb40f2e0c7839a2db4cce4c
deonvisser44 <51429368+deonvisser44@users.noreply.github.com>
2024-03-08 06:00:22 +0800

    chore: remove calendar notification body (#812)

--- cron-jobs/calendar-cron-job/notification-cron-job.ts
+++ cron-jobs/calendar-cron-job/notification-cron-job.ts
@@ -16,6 +16,8 @@ async function fetchEvents() {
   const timeInFiveMinutes = DateTime.now().plus({ minutes: 5 }).toJSDate();
   const events = await CronJobDataSource.manager.find(Notification, {
     where: { event_begins: Between(currentTime, timeInFiveMinutes), received: false },
+    relations: ['user'],
+    select: ['id', 'summary', 'description', 'event_begins', 'event_ends', 'user_id', 'user'],
   });
   const allExcludedKeywords = await CronJobDataSource.manager.find(CalendarExcludedKeyword, {
     where: {},
@@ -25,7 +27,13 @@ async function fetchEvents() {
       is_selected: true,
     },
   });
-  const eventsToSend = events.filter(async (event) => {
+  const eventsWithUserLanguage = events.map((event) => {
+    return {
+      ...event,
+      language: event.user.language,
+    };
+  });
+  const eventsToSend = eventsWithUserLanguage.filter(async (event) => {
     const excludedKeywords = allExcludedKeywords.filter((keyword) => {
       return keyword.user_id === event.user_id && keyword.platform === event.platform;
     });
@@ -58,24 +66,27 @@ const beamsClient = new PushNotifications({
   secretKey: process.env.PUSHER_BEAMS_PRIMARY_KEY,
 });
 
-const sendBeamsPushNotification = async (userId: string, notificationData: Notification) => {
+const NOTIFICATION_TITLES = {
+  en: 'New calendar notification',
+  es: 'Nueva notificación de calendario',
+};
+
+const sendBeamsPushNotification = async (userId: string, language: string, notificationData: Notification) => {
   try {
     const publishRequest: BeamsPublishRequest = {
       apns: {
         aps: {
           alert: {
-            title: notificationData.summary,
-            body: notificationData.description,
+            title: NOTIFICATION_TITLES[language],
+            body: notificationData.summary,
           },
         },
-        data: notificationData,
       },
       fcm: {
         notification: {
-          title: notificationData.summary,
-          body: notificationData.description,
+          title: NOTIFICATION_TITLES[language],
+          body: notificationData.summary,
         },
-        data: notificationData,
       },
     };
     await beamsClient.publishToUsers([userId], publishRequest);
@@ -97,7 +108,7 @@ const updateNotificationStatus = async (id: string) => {
     if (calendarEventsToSend.length === 0) process.exit();
     const sendNotificationsPromises = calendarEventsToSend.map(async (calendarEvent) => {
       const { id, summary, description, event_begins, event_ends } = calendarEvent;
-      await sendBeamsPushNotification(calendarEvent.user_id, {
+      await sendBeamsPushNotification(calendarEvent.user_id, calendarEvent.language, {
         id,
         summary,
         description,

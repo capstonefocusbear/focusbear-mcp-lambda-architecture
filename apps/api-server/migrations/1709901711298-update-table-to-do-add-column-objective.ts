b41398e96f9ea898b17c61302e8c6a090aa08d6e
amanuel <amanueltsfy@gmail.com>
2024-03-08 16:20:00 +0300

    chore(to-do): objective column added

---
+++ apps/api-server/migrations/1709901711298-update-table-to-do-add-column-objective.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UpdateTableToDoAddColumnObjective1709901711298 implements MigrationInterface {
+  name = 'UpdateTableToDoAddColumnObjective1709901711298';
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "to_do" ADD COLUMN "objective" character varying(32000);
+          `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "to_do" DROP COLUMN IF EXISTS "objective";
+          `);
+  }
+}

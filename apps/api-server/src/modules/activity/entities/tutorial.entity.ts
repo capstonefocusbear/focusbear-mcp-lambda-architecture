2d8ff783ec4680c45b6cceddfa433d5b871e8a71
amanuel <amanueltsfy@gmail.com>
2024-05-07 15:23:51 +0300

    fix: sonar issues resolved

--- apps/api-server/src/modules/activity/entities/tutorial.entity.ts
+++ apps/api-server/src/modules/activity/entities/tutorial.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn, Index, OneToOne, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, JoinColumn, Index, OneToOne, OneToMany } from 'typeorm';
 import { BaseEntity } from '../../../shared/entities/base-entity.entity';
 import { Activity } from './activity.entity';
 import { User } from '../../user/entities/user.entity';

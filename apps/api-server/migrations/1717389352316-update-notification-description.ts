7937ba4212a116d68d2f90d6a22faf19fb172b4f
Jeremy Nagel <jeremymnagel@gmail.com>
2024-06-03 16:30:13 +1000

    Update notification description field (#892)
    * fix too long value store
    
    * Update google-auth.service.ts
    
    * update description data type in notification table
    
    * update notification migration name
    
    ---------
    
    Co-authored-by: techcaptain83 <tech.captain83@gmail.com>
    Co-authored-by: techcaptain83 <126975345+techcaptain83@users.noreply.github.com>

--- apps/api-server/migrations/1717389352316-update-notification-description.ts
+++ apps/api-server/migrations/1717389352316-update-notification-description.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UpdateNotificationDescription1717389352316 implements MigrationInterface {
-  name = 'UpdateTableNotificationsIncreaseFieldCharacterLimits1714450883924';
+  name = 'UpdateNotificationDescription1717389352316';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE "notifications" ALTER COLUMN "description" TYPE TEXT;');

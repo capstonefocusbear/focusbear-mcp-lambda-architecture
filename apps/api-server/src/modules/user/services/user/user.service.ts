8642179c0df42a1465ec045e769a8d1b37996fab
amanueltsfy <amanueltsfy@gmail.com>
2024-06-21 16:43:48 +0300

    fix: lint issues resolved

--- apps/api-server/src/modules/user/services/user/user.service.ts
+++ apps/api-server/src/modules/user/services/user/user.service.ts
@@ -136,10 +136,12 @@ export class UserService {
           message: 'Registering new user in Stripe',
         });
 
-        const devicesFromDb = registeredUser ? await this.deviceRepository.orm.find({
-          where: { user_id: registeredUser.id },
-          order: { created_at: 'ASC' },
-        }) : [];
+        const devicesFromDb = registeredUser
+          ? await this.deviceRepository.orm.find({
+              where: { user_id: registeredUser.id },
+              order: { created_at: 'ASC' },
+            })
+          : [];
 
         const os =
           devicesFromDb.length > 0

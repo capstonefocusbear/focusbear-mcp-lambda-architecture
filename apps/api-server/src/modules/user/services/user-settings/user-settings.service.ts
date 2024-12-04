72473e2d2f2a2b80b103ac5d899d995baddfb49f
amanueltsfy <amanueltsfy@gmail.com>
2024-12-04 16:26:01 +0300

    chore: disable evening routine optimization during onboarding

--- apps/api-server/src/modules/user/services/user-settings/user-settings.service.ts
+++ apps/api-server/src/modules/user/services/user-settings/user-settings.service.ts
@@ -203,6 +203,7 @@ export class UserSettingsService {
       const { eveningActivities, is_relax_activity_generated } = await this.optimizeEveningActivities(
         updateSettingsData,
         user,
+        is_onboarding,
       );
 
       const updatedUser = new User({
@@ -572,14 +573,22 @@ export class UserSettingsService {
     return false;
   };
 
-  private async optimizeEveningActivities(updateSettingsData: UpdateUserSettingsDto, user: User) {
+  private async optimizeEveningActivities(
+    updateSettingsData: UpdateUserSettingsDto,
+    user: User,
+    is_onboarding?: boolean,
+  ) {
     let eveningActivities = [...updateSettingsData.evening_activities];
     let { is_relax_activity_generated } = user;
 
     // Check if user opts to re-add relax activity, even if previously generated
     const hasRelaxActivityInCurrent = eveningActivities.some((activity) => activity.show_saved_distracting_websites);
 
-    if (!updateSettingsData?.cutoff_time_for_non_high_priority_activities || hasRelaxActivityInCurrent) {
+    if (
+      is_onboarding ||
+      !updateSettingsData?.cutoff_time_for_non_high_priority_activities ||
+      hasRelaxActivityInCurrent
+    ) {
       return { eveningActivities, is_relax_activity_generated };
     }
 

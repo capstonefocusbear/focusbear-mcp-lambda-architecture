5308c0d4853a4a270596ab7100f39c2f841b9877
Amanuel T <amanueltsfy@gmail.com>
2025-05-07 16:21:54 +0300

    fix(server): ensure correct export name & use numeric port

--- apps/api-server/src/config/server.config.ts
+++ apps/api-server/src/config/server.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
-export const serverCongif = registerAs('server', () => ({
+export const serverConfig = registerAs('server', () => ({
   port: process.env.PORT || process.env.SERVER_PORT || 3009,
   host: process.env.SERVER_HOST || '127.0.0.1',
   frontEndUrl: process.env.SERVER_FRONTEND_URL,

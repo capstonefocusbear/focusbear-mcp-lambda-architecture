68f9918537e6e708490c76b1c4cb78c795fa6bd6
Amanuel T <amanueltsfy@gmail.com>
2025-05-07 18:20:52 +0300

    fix(server): prioritize PORT env var and update frontend env URLs

--- apps/api-server/src/config/server.config.ts
+++ apps/api-server/src/config/server.config.ts
@@ -1,7 +1,8 @@
 import { registerAs } from '@nestjs/config';
 
 export const serverConfig = registerAs('server', () => ({
-  port: process.env.PORT || process.env.SERVER_PORT || 3009,
-  host: process.env.SERVER_HOST || '127.0.0.1',
-  frontEndUrl: process.env.SERVER_FRONTEND_URL,
+  port: process.env.SERVER_PORT || 5038,
+  host: process.env.SERVER_HOST,
+  frontEndUrl: process.env.frontEndUrl,
+  devFrontendUrl: process.env.devFrontendUrl,
 }));
--- apps/api-server/src/main.ts
+++ apps/api-server/src/main.ts
@@ -30,8 +30,8 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter, { rawBody: true });
 
   const configService: ConfigService = app.get(ConfigService);
-  const PORT = Number(configService.get('server.port')) || 3009;
-  const HOST: string = configService.get('server.host');
+  const PORT = process.env.PORT || configService.get('server.port');
+  const HOST: string = configService.get('server.host') || '0.0.0.0';
   const HELMET: unknown = configService.get('helmet');
   const VALIDATION_PIPE: ValidationPipeOptions = configService.get('validation-pipe');
 

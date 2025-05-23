f1cc4050f6c920a06c81e6808a42f48cf42c45aa
ariefsaipul <143112736+ariefsaipul@users.noreply.github.com>
2025-05-23 11:03:13 +1000

    fix: resolve bug cron job (#1172)
    Co-authored-by: ariefsaipul <143112736+arieffrima@users.noreply.github.com>

--- cron-jobs/routine-notifications-cron-job/cron-job.ts
+++ cron-jobs/routine-notifications-cron-job/cron-job.ts
@@ -6,7 +6,7 @@ import OpenAI from 'openai';
 import { MoreThanOrEqual } from 'typeorm';
 // eslint-disable-next-line import/extensions
 import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
-import { GPT_4_1_MINI } from 'apps/api-server/src/shared/utils/constants';
+import { GPT_4_1_MINI } from '@api-server/shared/utils/constants';
 import { BeamsPublishRequest } from '../../libs/pusher-beams/src/domains/pusher-beams-publish-request.model';
 import { CronJobDataSource } from '../data-source';
 import { User } from '../../apps/api-server/src/modules/user/entities/user.entity';
@@ -14,6 +14,7 @@ import { ActivityType } from '../../apps/api-server/src/modules/activity/domain/
 import { openAiConfig } from '../../apps/api-server/src/config';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
+
 const OPEN_AI_CONFIG = openAiConfig();
 
 const MORNING_ROUTINE_TITLES = {

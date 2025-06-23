d650201e831ac88e62d9a1611b8d23ec38a0d014
Bao Ho Gia <54936754+BaoHG1508@users.noreply.github.com>
2025-06-23 09:21:48 +0700

    chore: update file name

--- apps/api-server/migrations/1710000000002-AddAfterFlankerGameScoreToStudyParticipants.ts
+++ apps/api-server/migrations/1710000000002-AddAfterFlankerGameScoreToStudyParticipants.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddFlankerEffectToStudyParticipants1710000000002 implements MigrationInterface {
+export class AddAfterStudyFlankerEffectToStudyParticipants1710000000002 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       ALTER TABLE study_participants

244222a4bd0d7d4305ec8508c629441d20b5d246
copypasteonly <denzeldadang@protonmail.com>
2025-12-01 15:52:56 +0800

    chore: remove wrong branch changes

--- apps/api-server/src/modules/activity-template/controllers/activity-library.controller.ts
+++ apps/api-server/src/modules/activity-template/controllers/activity-library.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Get, Post, Put, UseGuards, HttpCode } from '@nestjs/common';
 import { ApiSecurity, ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Throttle } from '@nestjs/throttler';
-import { R2Service } from '@app/r2';
 import { AuthContext } from '../../../shared/decorators/passport.decorator';
 import { UpdateActivityDto } from '../../activity/dto/update-activity.dto';
 import { Passport } from '../../auth/domain/passport.model';
@@ -23,7 +22,6 @@ export class ActivityLibraryController {
     private readonly activityLibraryService: ActivityLibraryService,
     private readonly routineSuggestionsAsyncService: RoutineSuggestionsAsyncService,
     private readonly habitCreationAsyncService: HabitCreationAsyncService,
-    private readonly r2Service: R2Service,
   ) {}
 
   @Get()

97022d6a9fa57fc7dadf65f74e414a25820bb1ff
copypasteonly <denzeldadang@protonmail.com>
2025-12-02 22:19:01 +0800

    chore: add client key payload

--- apps/api-server/src/modules/user/dto/is-app-safe.dto.ts
+++ apps/api-server/src/modules/user/dto/is-app-safe.dto.ts
@@ -1,5 +1,6 @@
 import { MAX_WORD_LENGTH } from '@app/openai/openai.constants';
 import { IsOptional, IsString, MaxLength } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class IsAppSafeDto {
   @IsOptional()
@@ -17,6 +18,7 @@ export class IsAppSafeDto {
 
   @IsOptional()
   @IsString()
+  @Transform(({ value, obj }) => value ?? obj?.justification ?? undefined)
   @MaxLength(MAX_WORD_LENGTH.justification)
   justificationForThisSpecificApp?: string;
 

55aa6c769b6f2d1c11f6202f1663aec03e432620
HuyNgocDuong <ngochuyduong288@gmail.com>
2025-12-17 23:55:14 +1100

    test: mock Sentry modules globally to prevent test failures

--- apps/api-server/test/mocks/index.ts
+++ apps/api-server/test/mocks/index.ts
@@ -1,3 +1,34 @@
 export * from './services.mock';
 export * from './repositories.mock';
 export * from './accountability-buddy.mocks';
+
+jest.mock('@sentry/nestjs', () => ({
+  init: jest.fn(),
+  captureException: jest.fn(),
+  captureMessage: jest.fn(),
+  withScope: jest.fn((callback) => {
+    const scope = {
+      setTag: jest.fn(),
+      setUser: jest.fn(),
+      setContext: jest.fn(),
+      setLevel: jest.fn(),
+    };
+    return callback(scope);
+  }),
+  cron: {
+    instrumentCron: jest.fn(),
+  },
+}));
+
+jest.mock('@sentry/nestjs/setup', () => ({
+  SentryModule: {
+    forRoot: jest.fn(() => ({
+      module: class MockSentryModule {},
+      providers: [],
+      exports: [],
+    })),
+  },
+  SentryGlobalFilter: jest.fn().mockImplementation(() => ({
+    catch: jest.fn(),
+  })),
+}));

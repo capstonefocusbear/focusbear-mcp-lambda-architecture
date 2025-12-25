71e9b12a080fb4ada2bee25a00f4a07a6d30d739
Quang Vinh <quangvinhle@outlook.com.au>
2025-12-26 10:03:40 +1100

    refactor: remove hardcoded auth0_id in main.seeder

--- apps/api-server/src/seeding/main.seeder.ts
+++ apps/api-server/src/seeding/main.seeder.ts
@@ -98,7 +98,6 @@ export class MainSeeder implements Seeder {
     // create dummy data for backendTestUser
     await userFactory.save({
       id: TEST_USER_ID,
-      auth0_id: 'auth0|643ca25c983376898fcd5028',
       username: 'testuser',
       num_days_of_stats: 30,
     });

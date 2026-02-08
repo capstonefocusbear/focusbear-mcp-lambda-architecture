2c8805e08c03088f6ac2f886535c3438815d4f0d
copypasteonly <denzeldadang@protonmail.com>
2026-02-08 19:33:32 +0800

    chore: add max limit and fix typing

--- apps/api-server/src/modules/to-do/dto/recent-to-do.dto.ts
+++ apps/api-server/src/modules/to-do/dto/recent-to-do.dto.ts
@@ -1,10 +1,10 @@
-import { ArrayNotEmpty, IsArray, IsDateString, IsEnum, IsNumber, IsOptional } from 'class-validator';
+import { ArrayNotEmpty, IsArray, IsDateString, IsEnum, IsNumber, IsOptional, Max } from 'class-validator';
 import { ToDoStatus } from '../domain/to-do-status.enum';
 
 export class RecentToDoDto {
   @IsOptional()
   @IsDateString()
-  updated_at: string;
+  updated_at?: string;
 
   @IsOptional()
   @IsArray()
@@ -14,5 +14,6 @@ export class RecentToDoDto {
 
   @IsOptional()
   @IsNumber()
+  @Max(100)
   take?: number = 50;
 }

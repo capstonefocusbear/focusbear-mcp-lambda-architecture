53786b1c4f835f2243e0d448250e51d511b5677d
copypasteonly <denzeldadang@protonmail.com>
2026-03-03 10:32:24 +0800

    fix: inheritance

--- apps/api-server/src/modules/to-do/dto/to-do-response.dto.ts
+++ apps/api-server/src/modules/to-do/dto/to-do-response.dto.ts
@@ -1,10 +1,31 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ToDo } from '../entities/to-do.entity';
-import { TaskCommentResponseDto } from './task-comment-response.dto';
 import { TaskCommentReactionResponseDto } from './task-comment-reaction-response.dto';
 import { TaskReactionResponseDto } from './task-reaction-response.dto';
+import { TaskCommentUserDto } from './task-comment-user.dto';
 
-export class TaskCommentWithReactionsDto extends TaskCommentResponseDto {
+export class TaskCommentWithReactionsDto {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  task_id: string;
+
+  @ApiProperty()
+  user_id: string;
+
+  @ApiProperty()
+  content: string;
+
+  @ApiProperty({ type: TaskCommentUserDto, required: false })
+  user?: TaskCommentUserDto;
+
+  @ApiProperty()
+  created_at: string;
+
+  @ApiProperty()
+  updated_at: string;
+
   @ApiProperty({ type: [TaskCommentReactionResponseDto], required: false })
   reactions?: TaskCommentReactionResponseDto[];
 }

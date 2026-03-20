f545e068b90ccf1247fc3c2b43b57d2512a79fac
Albert Novell Enabe <novell.enabe2004@gmail.com>
2026-03-20 10:46:33 +0800

    fix: added platform to query

--- apps/api-server/src/modules/integration/consumers/sync-tasks.consumer.ts
+++ apps/api-server/src/modules/integration/consumers/sync-tasks.consumer.ts
@@ -51,7 +51,7 @@ export class SyncTasksConsumer {
       const tasksFromProject = only_assigned
         ? await service.getTasksOwnedByUser(userId, portalId, projectId)
         : await service.getTasks(userId, portalId, projectId);
-      const syncedProjectRecord = await this.syncedProjectsService.getSyncedProject(projectId, userId);
+      const syncedProjectRecord = await this.syncedProjectsService.getSyncedProject(projectId, userId, platform);
 
       const tasksAsToDos = tasksFromProject.map((task) => {
         return new ToDo({
--- apps/api-server/src/modules/to-do/services/synced-projects.service.ts
+++ apps/api-server/src/modules/to-do/services/synced-projects.service.ts
@@ -53,10 +53,11 @@ export class SyncedProjectsService {
     ]);
   }
 
-  async getSyncedProject(externalProjectId: string, userId: string) {
+  async getSyncedProject(externalProjectId: string, userId: string, platform: string) {
     return this.syncedProjectsRepository.orm.findOneBy({
       external_project_id: externalProjectId,
       user_id: userId,
+      platform,
     });
   }
 
